bce4e96c3b44c5b106675c47be35ec4cc87272a1
gmldnjs26 <imleesky265@gmail.com>
2023-01-21 20:30:30 +0900

    config: dotEnv, add custom logging middleware

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
+    const envName = this.configService.get('NAME');
+    console.log(envName);
     return 'Hello World!';
   }
 }
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,23 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    // response로 응답이 처리될 떄 로깅한다. 위의 ip와 같은 정보들은 이미 요청이 들어왔을때 기록하고 있음
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
+    next();
+  }
+}
