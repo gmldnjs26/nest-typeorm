2b84a0410cf77ea3a54b377c1d83af5294841aff
gmldnjs26 <imleesky265@gmail.com>
2023-02-05 22:34:54 +0900

    config: sync typeorm to DB

---
+++ dataSource.ts
@@ -0,0 +1,34 @@
+import { DataSource } from 'typeorm';
+import { ChannelChats } from './src/entities/ChannelChats';
+import { ChannelMembers } from './src/entities/ChannelMembers';
+import { Channels } from './src/entities/Channels';
+import { DMs } from './src/entities/DMs';
+import { Mentions } from './src/entities/Mentions';
+import { Users } from './src/entities/Users';
+import { WorkspaceMembers } from './src/entities/WorkspaceMembers';
+import { Workspaces } from './src/entities/Workspaces';
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '5693',
+  database: 'sleact',
+  entities: [
+    ChannelChats,
+    ChannelMembers,
+    Channels,
+    DMs,
+    Mentions,
+    Users,
+    WorkspaceMembers,
+    Workspaces,
+  ],
+  migrations: [__dirname + '/src/migrations/*.ts'],
+  charset: 'utf8mb4_general_ci',
+  synchronize: false,
+  logging: true,
+});
+
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,15 @@ import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
 import { WorkspacesService } from './workspaces/workspaces.service';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelChats } from './entities/ChannelChats';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
+import { DMs } from './entities/DMs';
+import { Mentions } from './entities/Mentions';
+import { Users } from './entities/Users';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Workspaces } from './entities/Workspaces';
 
 @Module({
   imports: [
@@ -16,6 +25,28 @@ import { WorkspacesModule } from './workspaces/workspaces.module';
     DmsModule,
     ChannelsModule,
     WorkspacesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '5693',
+      database: 'sleact',
+      entities: [
+        ChannelChats,
+        ChannelMembers,
+        Channels,
+        DMs,
+        Mentions,
+        Users,
+        WorkspaceMembers,
+        Workspaces,
+      ],
+      synchronize: false, // 처음에 한번만 true하고 그 뒤 false
+      logging: true,
+      keepConnectionAlive: true, // HotReloading을 할 때 타입orm은 디비 연결을 끊어버리므로 연결을 유지할 수 있게 한다.
+      charset: 'utf8mb4_general_ci',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService, WorkspacesService],
